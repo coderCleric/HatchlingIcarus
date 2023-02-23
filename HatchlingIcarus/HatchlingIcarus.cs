5f91267f752de58cc5cf28670933844bc5c0a5ba
coderCleric <wescamphouse@gmail.com>
2023-02-22 22:01:27 -0700

    Add project files.

---
+++ HatchlingIcarus/HatchlingIcarus.cs
@@ -0,0 +1,92 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace HatchlingIcarus {
+    public class HatchlingIcarus : ModBehaviour
+    {
+        public static GameObject gravObject = null;
+        private int mode; //0 is feet, 1 is cam
+        public static float force;
+        private bool activeAlign;
+
+        /**
+         * Just need to load the patches
+         */
+        private void Start()
+        {
+            //Make all of the patches
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+            ModHelper.Console.WriteLine($"Hatchling Icarus is ready!");
+        }
+
+        /**
+         * Align the field to the player's rotation, taking the setting into accout
+         */
+        private void AlignField()
+        {
+            if(gravObject != null)
+            {
+                //To the body
+                if(mode == 0)
+                    gravObject.transform.rotation = Locator.GetPlayerTransform().rotation;
+
+                //To the camera
+                else
+                {
+                    gravObject.transform.rotation = Locator.GetPlayerCamera().transform.rotation;
+                    Vector3 rotationAxis = new Vector3(1, 0, 0);
+                    gravObject.transform.rotation *= Quaternion.AngleAxis(-90, rotationAxis);
+                }
+            }
+        }
+
+        /**
+         * Just need to check for the player pressing P & move the gravity object
+         */
+        private void Update()
+        {
+            if (gravObject != null)
+            {
+                //Enable/disable the field
+                if (Keyboard.current[Key.K].wasPressedThisFrame)
+                {
+                    gravObject.SetActive(!gravObject.activeSelf);
+                    gravObject.GetComponent<DirectionalForceVolume>().SetAttachedBody(Locator.GetSunTransform().GetComponent<OWRigidbody>());
+                    AlignField();
+                }
+
+                //Move it to the player
+                Transform playerTransform = Locator.GetPlayerTransform();
+                gravObject.transform.position = playerTransform.position;
+                if (activeAlign)
+                    AlignField();
+            }
+        }
+
+        /**
+         * Retrieve settings for the mod
+         */
+        public override void Configure(IModConfig config)
+        {
+            //Alignment mode
+            if (config.GetSettingsValue<string>("Alignment Mode").Equals("Body"))
+                mode = 0;
+            else
+                mode = 1;
+
+            //Force
+            force = config.GetSettingsValue<float>("Field Force") * 10.0f;
+            if (gravObject != null)
+                gravObject.GetComponent<DirectionalForceVolume>().SetFieldMagnitude(force);
+
+            //Active or not
+            activeAlign = config.GetSettingsValue<bool>("Active Alignment");
+        }
+    }
+}
+
---
+++ HatchlingIcarus/Patches.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace HatchlingIcarus
+{
+    [HarmonyPatch]
+    public static class Patches
+    {
+        /**
+         * When the locator is activated, make the gravity field
+         */
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Locator), nameof(Locator.LocateSceneObjects))]
+        public static void MakeGravField()
+        {
+            GameObject gravField = new GameObject();
+            gravField.SetActive(false);
+            gravField.layer = LayerMask.NameToLayer("BasicEffectVolume");
+
+            BoxCollider box = gravField.AddComponent<BoxCollider>();
+            box.isTrigger = true;
+
+            OWCollider OWCol = gravField.AddComponent<OWCollider>();
+            OWCol._lodActivationMask = new DynamicOccupantMask();
+
+            gravField.AddComponent<OWTriggerVolume>();
+
+            DirectionalForceVolume forceVolume = gravField.AddComponent<DirectionalForceVolume>();
+            forceVolume._priority = 99;
+            forceVolume._alignmentPriority = 99;
+            forceVolume._fieldDirection = Vector3.down;
+            forceVolume._fieldMagnitude = HatchlingIcarus.force;
+
+            HatchlingIcarus.gravObject = gravField;
+        }
+    }
+}
